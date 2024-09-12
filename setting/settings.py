df9a0558a624cdd2934ef151650acc3e0dbf5208
XumoyunX <xumoyuntoxtayev19@gmail.com>
2024-09-12 10:05:58 +0500

    Salom

--- setting/settings.py
+++ setting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j#8!-r($d+v_xlqea43jk+)#2s5mo9)(^4rrz7qh05^8z-1y#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
